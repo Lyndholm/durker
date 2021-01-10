8bf3ce71f6f8ac9177ba718fe594957fd9c8c614
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2021-01-10 19:01:07 +0300

    New checks
    New forbidden_channel() & forbidden_channels() checks

--- lib/utils/checks.py
+++ lib/utils/checks.py
@@ -17,10 +17,28 @@ def is_channel(channel: int):
 
 def is_any_channel(channels: List[int]):
     """
-    Similar to is_channel(), butchecks if the command is invoked in any allowed channel.
+    Similar to is_channel(), but checks if the command is invoked in any allowed channel.
     """
     def predicate(ctx):
         return any([ctx.message.channel.id == cid for cid in channels])
+    return commands.check(predicate)
+
+
+def forbidden_channel(channel: int):
+    """
+    A check() that checks if the command is invoked in forbidden channel. It can not be used wuth is_channel() or is_any_channel() methods.
+    """
+    def predicate(ctx):
+        return ctx.message.channel.id != channel
+    return commands.check(predicate)
+
+
+def forbidden_channels(channels: List[int]):
+    """
+    Similar to forbidden_channel(), but checks if the command is invoked in any forbidden channel.
+    """
+    def predicate(ctx):
+        return all([ctx.message.channel.id != cid for cid in channels])
     return commands.check(predicate)
 
 
