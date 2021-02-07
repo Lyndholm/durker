e146caadf68e740d8a9bc8d587e2f773c0fcaa98
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2021-02-07 20:53:14 +0300

    fn fish command

--- lib/cogs/fortniteapiio.py
+++ lib/cogs/fortniteapiio.py
@@ -1,3 +1,5 @@
+import json
+import aiofiles
 from os import getenv
 from aiohttp import ClientSession
 from discord import Embed, Color
@@ -44,7 +46,7 @@ class FortniteAPIio(Cog):
             usage=cmd["fnach"]["usage"],
             help=cmd["fnach"]["help"],
             hidden=cmd["fnach"]["hidden"], enabled=True)
-    async def show_fortnite_achievements_command(self, ctx, language="ru"):
+    async def show_fortnite_achievements_command(self, ctx, language: str ="ru"):
         achievement_embeds = []
         divided = []
 
@@ -77,6 +79,73 @@ class FortniteAPIio(Cog):
         page = Paginator(self.bot, message, only=ctx.author, use_more=True, embeds=achievement_embeds)
         await page.start()
 
+
+    @command(name=cmd["fish"]["name"], aliases=cmd["fish"]["aliases"], 
+            brief=cmd["fish"]["brief"],
+            description=cmd["fish"]["description"],
+            usage=cmd["fish"]["usage"],
+            help=cmd["fish"]["help"],
+            hidden=cmd["fish"]["hidden"], enabled=True)
+    async def show_fortnite_fish_list_command(self, ctx, number: int = 0, language: str ="ru"):
+        async with aiofiles.open('./data/fish.json', mode='r', encoding='utf-8') as f:
+            data = json.loads(await f.read())
+
+        if number == 0:
+            fish_embeds = []
+            for count, entry in enumerate(data["fish"]):
+                embed = Embed(
+                    title=entry['name'] + f" | {str(count+1)}",
+                    color=Color.blue(),
+                    description=entry['description'],
+                )
+                embed.set_thumbnail(url=entry['image'])
+
+                fields = [
+                    ("Редкость", entry["rarity"], True),
+                    ("Подробности", entry["details"], True),
+                    ("Требуется проф. удочка", "Да" if entry["needsProFishingRod"] else "Нет", True),
+                    ("Минимальный размер", entry["sizeMin"], True),
+                    ("Максимальный размер", entry["sizeMax"], True),
+                    ("Максимум в слоте", entry["maxStackSize"], True)
+                ]
+
+                for name, value, inline in fields:
+                    embed.add_field(name=name, value=value, inline=inline)
+
+                fish_embeds.append(embed)
+
+            message = await ctx.send(embed=fish_embeds[0])
+            page = Paginator(self.bot, message, only=ctx.author, embeds=fish_embeds)
+            await page.start()
+            return
+
+        else:
+            if number > len(data['fish']) or number < 0:
+                embed = Embed(title='Некорректный номер рыбки!', color= Color.red())
+                await ctx.message.reply(embed=embed)
+                return
+            number-=1
+            embed = Embed(
+                    title=data['fish'][number]['name'] + f" | {str(number+1)}",
+                    color=Color.blue(),
+                    description=data['fish'][number]['description']
+                )
+            embed.set_thumbnail(url=data['fish'][number]['image'])
+
+            fields = [
+                    ("Редкость", data['fish'][number]["rarity"], True),
+                    ("Подробности", data['fish'][number]["details"], True),
+                    ("Требуется проф. удочка", "Да" if data['fish'][number]["needsProFishingRod"] else "Нет", True),
+                    ("Минимальный размер", data['fish'][number]["sizeMin"], True),
+                    ("Максимальный размер", data['fish'][number]["sizeMax"], True),
+                    ("Максимум в слоте", data['fish'][number]["maxStackSize"], True)
+                ]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+            await ctx.send(embed=embed)
+                
 
 def setup(bot):
     bot.add_cog(FortniteAPIio(bot))
