da27f2b1326652e26f42751401d053697c220d57
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2021-07-05 13:10:56 +0300

    Fix time in on_bulk_message_delete

--- lib/cogs/audit_logging.py
+++ lib/cogs/audit_logging.py
@@ -116,16 +116,15 @@ class Audit(Cog, name='Система Аудита'):
                             color=Color.red(), timestamp=datetime.utcnow())
                 async with aiofiles.open(f'./data/audit/bulk-deleted-messages/{messages[0].id}.log', mode='w', encoding='utf-8') as f:
                     nl = '\n'
-                    time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
                     for msg in messages:
                         if msg.attachments:
                             attachments_url = [attachment for attachment in msg.attachments]
 
-                            await f.write(f"{time} | User: {msg.author} | UserID = {msg.author.id}" + "\n" +
+                            await f.write(f"{msg.created_at.strftime('%d.%m.%Y %H:%M:%S')} | User: {msg.author} | UserID = {msg.author.id}" + "\n" +
                                 f"MessageID = {msg.id}\nMessage content: {msg.clean_content}" + "\n" +
                                 f"Attachments: {nl.join([url.proxy_url for url in attachments_url]) + nl + nl.join([url.url for url in attachments_url])}" "\n\n")
                         else:
-                            await f.write(f"{time} | User: {msg.author} | UserID = {msg.author.id}" + nl +
+                            await f.write(f"{msg.created_at.strftime('%d.%m.%Y %H:%M:%S')} | User: {msg.author} | UserID = {msg.author.id}" + nl +
                                 f"MessageID = {msg.id}\nMessage content: {msg.clean_content}" + "\n\n")
 
                 embed.add_field(name="Канал:", value=f"{messages[0].channel.name} ({messages[0].channel.mention})")
