4291028d6bf7a1d8cad7050e54557a34216ba0a4
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-01-15 16:24:46 +0300

    Remove fortnite item shop command

--- lib/cogs/background_tasks.py
+++ lib/cogs/background_tasks.py
@@ -1,22 +1,15 @@
-import json
-import os
 from datetime import datetime
 from itertools import cycle
 
-import aiofiles
-from aiohttp import ClientSession
 from discord import Activity, ActivityType
 from discord.ext import tasks
 from discord.ext.commands import Cog
 from discord.utils import get
-from jishaku.functools import executor_function
 
-from ..db import db
 from ..utils.constants import (CAPTAIN_ROLE_ID, OLD_ROLE_ID, VETERAN_ROLE_ID,
                                WORKER_ROLE_ID)
 from ..utils.utils import edit_user_reputation
 
-ITEM_SHOP_ENDPOINT = "https://fortnite-api.com/v2/shop/br/combined"
 ACTIVITIES = cycle([
     '+help | durker.fun',
     '+help | docs.durker.fun',
@@ -31,7 +24,6 @@ class BackgroundTasks(Cog, name='Фоновые процессы'):
         self.change_bot_activity.start()
         self.check_activity_role.start()
         self.update_user_nickname.start()
-        self.update_fortnite_shop_hash.start()
         if self.bot.ready:
             bot.loop.create_task(self.init_vars())
 
@@ -117,46 +109,6 @@ class BackgroundTasks(Cog, name='Фоновые процессы'):
     async def before_update_user_nickname(self):
         await self.bot.wait_until_ready()
 
-
-    @executor_function
-    def create_item_shop_image(self, data: dict):
-        if data['len'] <= 59:
-            os.system("python ./athena/athena.py")
-        else:
-            os.system("python ./athena/athena_monotonic.py")
-
-    @tasks.loop(minutes=1.0)
-    async def update_fortnite_shop_hash(self):
-        """Makes a request to the api and checks if the hash has been updated"""
-        async with ClientSession() as session:
-            async with session.get(url=ITEM_SHOP_ENDPOINT) as r:
-                if r.status != 200:
-                    return
-
-                data = await r.json()
-                new_hash = data['data']['hash']
-                new_len = len(data['data']['featured']['entries']) + len(data['data']['daily']['entries'])
-                async with aiofiles.open('athena/athena_cache.json', mode='r', encoding='utf-8') as f:
-                    cached_data = json.loads(await f.read())
-                    cached_hash = cached_data['hash']
-                    cached_len = cached_data['len']
-
-        if new_hash != cached_hash or new_len != cached_len:
-            date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-            cache = {"hash": new_hash, "date":date, "len": new_len}
-
-            async with aiofiles.open('athena/athena_cache.json', 'w', encoding='utf-8') as f:
-                await f.write(json.dumps(cache, indent=2, sort_keys=True, ensure_ascii=False))
-
-            await self.create_item_shop_image(data=cache)
-            date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-            await self.bot.logs_channel.send(
-                f"Shop updated & rendered for `{date}` | `{cache['hash']}` | `{cache['len']}`"
-            )
-
-    @update_fortnite_shop_hash.before_loop
-    async def before_update_shop_hash(self):
-        await self.bot.wait_until_ready()
 
 async def setup(bot):
     await bot.add_cog(BackgroundTasks(bot))
